6c4574d9d2c6c1ab3924e161d4b36df936d94ed5
idilettant <dedenev1985@gmail.com>
2021-09-12 21:47:54 +0300

    Add cropping result

--- projects/avatar_cropping/main.py
+++ projects/avatar_cropping/main.py
@@ -1,22 +1,26 @@
 from PIL import Image
 
+def cropp_avatar():
+    image_monroe = Image.open('monro.jpg')
+    red_channel, green_channel, blue_channel = image_monroe.split()
 
-image_monroe = Image.open('monro.jpg')
-red_channel, green_channel, blue_channel = image_monroe.split()
+    cutting_width = 50
 
-cutting_width = 50
+    red_channel_1 = red_channel.crop((cutting_width, 0, red_channel.width, red_channel.height))
+    red_channel_2 = red_channel.crop((cutting_width / 2, 0, red_channel.width - cutting_width / 2, red_channel.height))
+    blended_red_channel = Image.blend(red_channel_1, red_channel_2, 0.5)
 
-red_channel_1 = red_channel.crop((cutting_width, 0, red_channel.width, red_channel.height))
-red_channel_2 = red_channel.crop((cutting_width / 2, 0, red_channel.width - cutting_width / 2, red_channel.height))
-blended_red_channel = Image.blend(red_channel_1, red_channel_2, 0.5)
+    blue_channel_1 = red_channel.crop((0, 0, blue_channel.width - cutting_width, blue_channel.height))
+    blue_channel_2 = blue_channel.crop((cutting_width / 2, 0, blue_channel.width - cutting_width / 2, blue_channel.height))
+    blended_blue_channel = Image.blend(blue_channel_1, blue_channel_2, 0.5)
 
-blue_channel_1 = red_channel.crop((0, 0, blue_channel.width - cutting_width, blue_channel.height))
-blue_channel_2 = blue_channel.crop((cutting_width / 2, 0, blue_channel.width - cutting_width / 2, blue_channel.height))
-blended_blue_channel = Image.blend(blue_channel_1, blue_channel_2, 0.5)
+    cropped_green_channel = green_channel.crop((cutting_width / 2, 0, green_channel.width - cutting_width / 2, green_channel.height))
 
-cropped_green_channel = green_channel.crop((cutting_width / 2, 0, green_channel.width - cutting_width / 2, green_channel.height))
+    blurry_image = Image.merge('RGB', (blended_red_channel, cropped_green_channel, blended_blue_channel))
+    blurry_image.save('blurry_image.jpg')
+    blurry_image.thumbnail((80, 80), Image.ANTIALIAS)
+    blurry_image.save('blurry_avatar.jpg')
 
-blurry_image = Image.merge('RGB', (blended_red_channel, cropped_green_channel, blended_blue_channel))
-blurry_image.save('blurry_image.jpg')
-blurry_image.thumbnail((80, 80), Image.ANTIALIAS)
-blurry_image.save('blurry_avatar.jpg')
+
+if __name__ == '__main__':
+    cropp_avatar()
